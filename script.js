a226e9fecfdcf9878631371525e50c350a51cc80
KMELLO3A <kethylynnascimento01@gmail.com>
2024-11-09 23:52:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const avanca = document.querySelectorAll(".btn_proximo")
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo')
+        const proximoPasso = 'passo_' + this.getAttribute("data_proximo")
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo')
+    })
+})
